25a9e9dd6eecb04f62b9ce04d004bdce0ab76dc5
Raul <raul.bricio@gmail.com>
2024-03-18 10:57:29 +0100

    First Commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/S5/',
+  base: '/S5.-Onboarding-digital/',
   plugins: [react()],
   publicDir: 'public',
 })
